94b019d56e1fe434f60adcda5a84d5bc8abfd230
ratul <ratuljh@gmail.com>
2018-05-07 23:54:10 +0600

    Initial commit

--- src/categories/models.py
+++ src/categories/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
+class Category(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=10, default=00.00)
 
     def __str__(self):
         return self.title
